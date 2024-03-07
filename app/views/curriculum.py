78e33a6e81a312484a45bd693ab386dc78c53836
Dominic Canora <dominiccanora@gmail.com>
2024-03-07 15:57:07 -0500

    QR Code

--- app/views/curriculum.py
+++ app/views/curriculum.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "app/curriculum_night/2024.html")
+    return render(request, "app/curriculum_night/2024.html", {
+        "qr": "qr" in request.GET
+    })
 
 
 def index_dark(request):
