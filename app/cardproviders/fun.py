e68d7f1696dc7d3d5c9f1379317d25a4623e38f3
Dominic Canora <dominiccanora@gmail.com>
2024-05-17 12:10:29 -0400

    Wrapped button on homepage

--- app/cardproviders/fun.py
+++ app/cardproviders/fun.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.utils import timezone
 
-from app.models import Student
+from app.models import Student, FeatureFlag, Wrapped2024
 
 
 def allcards(request):
-    return [x for x in [bday_self(request), bday_others(request)] if x is not None]
+    return [x for x in [wrapped2024(request), bday_self(request), bday_others(request)] if x is not None]
 
 
 def bday_self(request):
@@ -34,3 +34,10 @@ def bday_others(request):
                                 request=request)
     else:
         return None
+
+
+def wrapped2024(request):
+    enabled, _ = FeatureFlag.objects.get_or_create(id='card_wrapped')
+
+    if enabled and Wrapped2024.objects.filter(student=request.user.student).exists():
+        return render_to_string("app/cards/wrapped.html", request=request)
