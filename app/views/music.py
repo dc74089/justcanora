0fb62479590aaa29ac8c925013f70924993ca968
Dominic Canora <dominiccanora@gmail.com>
2024-11-07 09:23:23 -0500

    CSRF

--- app/views/music.py
+++ app/views/music.py
@@ -74,6 +74,7 @@ def do_play_pause(request):
         return HttpResponse(status=200)
 
 
+@csrf_exempt
 def do_skip(request):
     if request.headers.get('key', "") == 'oinkoinkboom':
         nowplaying.next_track(request)
