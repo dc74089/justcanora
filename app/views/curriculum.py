e1f128189475a2def5a487851e01bd750170772b
Dominic Canora <dominiccanora@gmail.com>
2025-01-21 12:58:58 -0500

    Curriculum Night update

--- app/views/curriculum.py
+++ app/views/curriculum.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "app/curriculum_night/2024.html", {
+    return render(request, "app/curriculum_night/cn.html", {
         "qr": "qr" in request.GET
     })
 
@@ -12,4 +12,4 @@ def big_qr(request):
 
 
 def index_dark(request):
-    return render(request, "app/curriculum_night/2024-darkmode.html")
+    return render(request, "app/curriculum_night/darkmode.html")
