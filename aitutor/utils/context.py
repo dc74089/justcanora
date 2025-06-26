7601ff9bba7193de7f5f80eb83a7bb17eac1bd6b
Dominic Canora <dominiccanora@gmail.com>
2025-06-26 19:28:47 -0400

    Filter tutor availability by student course languages in context processor

--- aitutor/utils/context.py
+++ aitutor/utils/context.py
@@ -4,6 +4,17 @@ from app.models import FeatureFlag
 def context_processor(request):
     ff, _ = FeatureFlag.objects.get_or_create(id="tutor_available")
 
+    languages = []
+
+    try:
+        if request.user.is_authenticated:
+            if 'APCSA' in request.user.student.courses.values_list('type', flat=True):
+                languages.append("java")
+            if 'CS2' in request.user.student.courses.values_list('type', flat=True):
+                languages.append("python")
+    except:
+        pass
+
     return {
-        "tutor_available": bool(ff)
+        "tutor_available": bool(ff) and len(languages) > 0,
     }
