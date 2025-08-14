e17e2cc4a26e847dd552fbb60f08878bee733857
Dominic Canora <dominiccanora@gmail.com>
2025-08-13 21:00:10 -0400

    Refine music suggestion logic to include grades 9-12 and exclude others

--- app/cardproviders/music.py
+++ app/cardproviders/music.py
@@ -22,7 +22,7 @@ def suggestion(request):
     if not enabled: return None
     if spotify.needs_login(request): return None
 
-    if not request.user.student.grade or request.user.student.grade > 8:
+    if not request.user.student.grade or 9 <= request.user.student.grade <= 12:
         return None
 
     msq = MusicSuggestion.objects.filter(student=request.user.student).order_by('-added')
